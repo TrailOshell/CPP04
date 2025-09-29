b4a48a1d4a2cb759a6f8053abe7bbee767b85cc7
TrailOshell <TrailOshell.com>
2025-09-29 18:38:12 +0700

    ex00 test between Animal and WrongAnimal

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 13:49:27 by tsomchan          #+#    #+#             */
-/*   Updated: 2025/09/29 18:17:26 by tsomchan         ###   ########.fr       */
+/*   Updated: 2025/09/29 18:36:32 by tsomchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,55 @@ int main()
 	meta->makeSound();
 
 	//...
+	
+	std::cout << std::endl;
+	std::cout << PUR "------------------------------------------------------------------------------" NCL << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << GRN "const " BLU"Animal" GRN"* animal = new " BLU"Animal" GRN"();" NCL << std::endl;
+	const Animal* animal = new Animal();
+	std::cout << animal->getType() << ": ";
+	animal->makeSound();
+	
+	std::cout << std::endl;
+	
+	std::cout << GRN "const " YLW"Cat" GRN"* cat = new " YLW"Cat" GRN"();" NCL << std::endl;
+	const Cat* cat = new Cat();
+	std::cout << cat->getType() << ": ";
+	cat->makeSound();
+	
 	std::cout << std::endl;
 
-	std::cout << YLW "const WrongAnimal* wronganimal = new WrongAnimal();" NCL << std::endl;
+	std::cout << GRN "const " BLU"Animal" GRN"* normal_cat = new " YLW"Cat" GRN"();" NCL << std::endl;
+	const Animal* normal_cat = new Cat();
+	std::cout << normal_cat->getType() << ": ";
+	normal_cat->makeSound();
+	
+	std::cout << std::endl;
+	std::cout << PUR "------------------------------------------------------------------------------" NCL << std::endl;
+	std::cout << std::endl;
+
+	std::cout << GRN "const " BLU"WrongAnimal" GRN"* wronganimal = new " BLU"WrongAnimal" GRN"();" NCL << std::endl;
 	const WrongAnimal* wronganimal = new WrongAnimal();
 	std::cout << wronganimal->getType() << ": ";
 	wronganimal->makeSound();
 
 	std::cout << std::endl;
 
-	std::cout << YLW "const WrongCat* wrongcat = new WrongCat();" NCL << std::endl;
+	std::cout << GRN "const " YLW"WrongCat" GRN"* wrongcat = new " YLW"WrongCat" GRN"();" NCL << std::endl;
 	const WrongCat* wrongcat = new WrongCat();
 	std::cout << wrongcat->getType() << ": ";
 	wrongcat->makeSound();
 	
 	std::cout << std::endl;
 
-	std::cout << YLW "const WrongAnimal* weird_cat = new WrongCat();" NCL << std::endl;
+	std::cout << GRN "const " BLU"WrongAnimal" GRN"* weird_cat = new " YLW"WrongCat" GRN"();" NCL << std::endl;
 	const WrongAnimal* weird_cat = new WrongCat();
 	std::cout << weird_cat->getType() << ": ";
 	weird_cat->makeSound();
 
 	std::cout << std::endl;
+	std::cout << PUR "------------------------------------------------------------------------------" NCL << std::endl;
 
 	if (DEBUG_MODE != 0)
 	{
